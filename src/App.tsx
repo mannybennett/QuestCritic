5bea95d2dbc5617fe1d3c9ec03de4bb80b5f5461
mannybennett <mannybennett99@gmail.com>
2024-02-07 23:46:52 -0500

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import darKLogo from './assets/qcLogoDark.png';
+import whiteLogo from './assets/qcLogoWhite.png';
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
 
   return (
-    <>
-    </>
+    <main className={darkMode ? 'dark' : 'light'}>
+      <div className='main-container'>
+        <header>
+          <span className='box'></span>
+          <span className='box title'>
+            <img src={darkMode ? whiteLogo : darKLogo} alt="logo" />
+            <h1>QuestCritic</h1>
+          </span>
+          <span className='box log-reg'>
+            <p className='login'>LOGIN</p>
+            <div className='reg-container'>
+              <p className='register'>REGISTER</p>
+            </div>
+          </span>
+        </header>
+      </div>
+    </main>
   );
 
 };
